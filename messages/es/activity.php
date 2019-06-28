15d331f1d39a5fc3a3483a500c480c508127ccb2
oscar-commits <52315590+oscar-commits@users.noreply.github.com>
2019-06-28 12:34:37 +0200

    Create activity.php
    Spanish translations for activities.php

---
+++ messages/es/activity.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Message translations.
+ *
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * Message string can be used with plural forms format. Check i18n section
+ * of the guide for details.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Activities' => 'Actividades',
+    'Backup failed: backup aborted.' => 'Copia de respaldo falló: copia suspendida.',
+    'Backup failed: network error.' => 'Copia de respaldo falló: Error de la red.',
+    'Backup failed: rdiff-backup failed (retval: {retval})' => 'Copia de respaldo falló: rdiff-backup falló (Retorna: {retval})',
+    'Client state changed: {client_state}' => 'El estado del cliente ha cambiado: {client_state}',
+    'Client state changed: {old} -> {new}' => 'El estado del cliente ha cambiado: {old} -> {new}',
+    'Date' => 'Fecha',
+    'Description' => 'Descripción ',
+    'Download failed: network error.' => 'Falló la descarga: Error de la red.',
+    'ID' => 'ID',
+    'No activities found.' => 'No hemos encontrado actividades.',
+    'Restore failed: rdiff-backup failed (retval: {retval})' => 'La recuperación falló: rdiff-backup falló (Retornat: {retval})',
+    'Severity' => 'Gravedad',
+    'Ticket' => 'Ticket',
+    'new activities; click to load' => 'Nuevas actividades; Haga clic para cargar',
+    '"Bootup successful" message was not recieved, but client is successfully booted up. Client is now unlocked.' => '"Inicio del sistema fue exitoso" el mensage no fue recibido pero el cliente fue correctamente iniciado. El está habilitado',
+    'Backup failed: leaving ticket in abandoned state.' => 'Copia de respaldo falló: hemos abandonado el ticket',
+    'Exam download successfully requested by {ip} from Ticket with token {token}.' => 'La descarga del examen fue exitosamente solicitada por {ip} de el ticket con token {token}',
+    'Exam download successfully requested by {ip} from {test_taker}.' => 'La descarga del examen fue exitosamente solicitada por {ip} de {test_taker}',
+    'Exam finished by Ticket with token {token}.' => 'Examen terminado por el ticket con token {token}',
+    'Exam finished by {test_taker}.' => 'Examen terminado por {test_taker}',
+    'Exam result handed in.' => 'Examen entregado.',
+    'Reset Grid' => 'Restablecer la red.',
+    'Restore failed: "{file}": No such file or directory.' => 'La recuperación falló: Archivo no se encuentra en al carpeta',
+    'Restore failed: network error.' => 'La recuperación falló: Error de la red',
+    'Restore of {file} as it was as of {date} was successful.' => 'La recuperación del archivo {file} como se encontraba en la fecha {date} fue exitosa.',
+    'Download failed: rsync failed (retval: {retval})' => '@@@@',
+    'Exam download aborted (server side).' => '@@@@',
+    'Exam download finished by {ip} from Ticket with token {token}.' => '@@@@',
+    'Exam download finished by {ip} from {test_taker}.' => '@@@@',
+    'backup aborted.' => '@@@@',
+    'backup successful.' => '@@@@',
+];
